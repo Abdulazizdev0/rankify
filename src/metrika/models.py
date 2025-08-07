fe9063fcd07a077837c2eaf1b30b84241447d470
abdulaziz <abdulazizmukhammadyunus@gmail.com>
2025-08-07 11:55:15 +0500

    create apps

---
+++ src/metrika/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# metrika,studentmetrika
--- src/rankify/settings.py
+++ src/rankify/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'result',
+    'metrika',
+    'course',
+    'notification',
+    'user',
+
 ]
 
 MIDDLEWARE = [
